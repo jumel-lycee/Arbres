86c18ff679584d6def94a155fd117fabc5161253
Vincent-Xavier JUMEL <vincent.xavier.jumel@lasallesaintdenis.com>
2020-11-19 22:09:20 +0100

    Ajout des tests

--- tests/test_noeud.py
+++ tests/test_noeud.py
@@ -1,7 +1,42 @@
-import pytest
-
 from arbre.main import Noeud
+
 
 def test_racine():
     racine = Noeud("A")
 
+    assert racine.valeur == "A"
+
+
+def test_sous_arbre_gauche():
+    racine = Noeud("A")
+    sous_arbre_gauche = racine.cree_fils_gauche("B")
+    sous_arbre_gauche.cree_fils_gauche("D")
+
+    assert sous_arbre_gauche.gauche.valeur == "D"
+
+
+def test_arbre():
+    racine = Noeud("A")
+    sous_arbre_gauche = racine.cree_fils_gauche("B")
+    sous_arbre_gauche.cree_fils_gauche("D")
+    racine.cree_fils_droit("C")
+
+    assert not racine.est_feuille()
+
+
+def test_arbre_droit():
+    racine = Noeud("A")
+    sous_arbre_gauche = racine.cree_fils_gauche("B")
+    sous_arbre_gauche.cree_fils_gauche("D")
+    racine.cree_fils_droit("C")
+
+    assert racine.droit.est_feuille()
+
+
+def test_arbre_gauche_valeur():
+    racine = Noeud("A")
+    sous_arbre_gauche = racine.cree_fils_gauche("B")
+    sous_arbre_gauche.cree_fils_gauche("D")
+    racine.cree_fils_droit("C")
+
+    assert racine.gauche.valeur == "B"
